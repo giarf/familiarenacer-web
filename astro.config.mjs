af47133ac79b14772e0d6c49f65135f433d5ffb1
giarf <gabriel@giarf.cl>
2026-01-13 18:09:52 -0300

    deje la embarra

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [tailwind()],
+
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
