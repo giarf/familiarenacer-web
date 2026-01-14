280cbf167de16c90cf44b75d6617919c1c584bfe
giarf <gabriel@giarf.cl>
2026-01-13 22:54:10 -0300

    feat: actualice 3D Object

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
+  // Solo usamos la integración oficial (compatible con Tailwind v3)
   integrations: [tailwind()],
+
+  // Importante para SSR
+  output: 'server',
 
   adapter: node({
     mode: 'standalone'
   }),
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
 });
