e278ecde92a2326470526f04dd60b585f096c6e0
giarf <gabriel@giarf.cl>
2026-03-16 23:51:58 -0300

    feat: Implement monthly collaborator page and component with custom global styling, replacing Tailwind CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({
-  // Solo usamos la integración oficial (compatible con Tailwind v3)
-  integrations: [tailwind()],
-
-  // Importante para SSR
-  output: 'server',
-
-  adapter: node({
-    mode: 'standalone'
-  }),
-});
+export default defineConfig({});
